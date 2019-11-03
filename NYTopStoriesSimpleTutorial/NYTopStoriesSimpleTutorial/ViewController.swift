f22c6fdd2aa596254dac44da369b8d28c9dec3cf
Ahmed Labib <a.a.labib@daimler.com>
2019-11-03 15:19:04 +0100

    Initial New project

---
+++ NYTopStoriesSimpleTutorial/NYTopStoriesSimpleTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NYTopStoriesSimpleTutorial
+//
+//  Created by Ahmed Labib on 03.11.19.
+//  Copyright © 2019 Ahmed Labib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
